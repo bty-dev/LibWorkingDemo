78b93f65ce726ce9f479c8d89b5e56266c3bce65
bty-dev <81759658+bty-dev@users.noreply.github.com>
2022-10-08 14:30:40 +0500

    Добавьте файлы проекта.

---
+++ AgeLimiterLibrary/AgeLimiter.cs
@@ -0,0 +1,20 @@
+﻿namespace AgeLimiterLibrary
+{
+    public class AgeLimiter
+    {
+        int age;
+
+        public AgeLimiter(int age)
+        {
+            this.age = age;
+        }
+        public void checkAge() => Console.WriteLine($"Your age is: {age}");
+
+        public string amIOld()
+        {
+            var result = this.age > 18 ? "You old!" : "You young!";
+            return result;
+        }
+
+    }
+}
---
+++ LibWorkingDemo/Program.cs
@@ -0,0 +1,6 @@
+﻿using AgeLimiterLibrary;
+
+AgeLimiter ageLim = new AgeLimiter(19);
+ageLim.checkAge();
+var ageCategory = ageLim.amIOld();
+Console.WriteLine(ageCategory);
